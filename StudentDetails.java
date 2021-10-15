bd6431b96d776c9c8c15c0fcb3e4934cbfacc31a
Om Purani 119 <87861143+OmPurani119@users.noreply.github.com>
2021-10-15 08:23:27 -0400

    Update StudentDetails.java

--- StudentDetails.java
+++ StudentDetails.java
@@ -2,9 +2,9 @@ public class StudentDetails {
 
 	    public static void main(String arg[])
 	    {
-	        String names[] = { "Jordan", "Sam", "James" };
 	        int marks[] = { 98, 88, 83};
 	        char sections[] = { 'A', 'B', 'A'};
+		String names[] = { "Jordan", "Sam", "James" };
 	        
 	        for(int i = 0; i < names.length; i++)
 	        {
